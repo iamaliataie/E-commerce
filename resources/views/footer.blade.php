0e871c74e96672dae578d3afd54106fd6997671b
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2021-03-14 15:33:55 +0430

    footer update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,6 +2,7 @@
     <div class="container">
         <div class="footer text-center text-white">
             <h4>by Ali Ahmad Ataie</h4>
+            <p>www.terabyte.af</p>
         </div>
     </div>
 </div>
