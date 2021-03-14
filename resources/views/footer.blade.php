9ed453e3b0ac54b01d94d29ddf54fbd2603ab44c
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2021-03-14 15:22:23 +0430

    header footer login page

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="container-fluid bg-primary p-3">
+    <div class="container">
+        <div class="footer text-center text-white">
+            <h4>by Ali Ahmad Ataie</h4>
+        </div>
+    </div>
+</div>
